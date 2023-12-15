45d5ee927af483cf6b56a86d2079034e6088d358
Eben Bruyns <eben@sdk.co.nz>
2023-12-16 08:40:50 +1300

    fixed flatpak issue to resolve login issue with legendary

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 if flatpak list | grep -q "com.github.derrod.legendary"; then
-    echo "legendary flatpak is installed"
-else
+    echo "legendary flatpak is installed, removing and reinstalling"
+    flatpak uninstall com.github.derrod.legendary -y
     cd /tmp
-    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.1/legendary.flatpak
+    flatpak --user install flathub org.gnome.Platform//45 -y
+    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.2/legendary.flatpak
     flatpak --user install legendary.flatpak -y
     rm legendary.flatpak
-fi
+else
+    cd /tmp
+    flatpak --user install flathub org.gnome.Platform//45 -y
+    wget https://github.com/ebenbruyns/legendary-flatpak/releases/download/Test-0.2/legendary.flatpak
+    flatpak --user install legendary.flatpak -y
+    rm legendary.flatpakfi
 
 if flatpak list | grep -q "io.github.dosbox-staging"; then
     echo "dosbox-staging flatpak is installed"
